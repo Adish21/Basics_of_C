73a8a7ae544613b10f9ca77dcf54a77ad401e867
ADISH BAHATKAR <97829093+Adish21@users.noreply.github.com>
2022-10-02 22:29:41 +0530

    Update Calculaor.c

--- Calculaor.c
+++ Calculaor.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include <stdio.h>   //Header file
 
-int main() {
-
-  char op;
-  double first, second;
-  printf("Enter an operator (+, -, *, /): ");
+int main() {        //main function
+  //Variable declaration
+  char op;          //charecter  declaration
+  double first, second;   //double declaration
+  printf("Enter an operator (+, -, *, /): "); //Input for operator
   scanf("%c", &op);
-  printf("Enter two operands: ");
+  printf("Enter two operands: ");     //Input operands
   scanf("%lf %lf", &first, &second);
-
+    //Apply switch case for the calculator
   switch (op) {
     case '+':
       printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
